cadd9f8db61232b2c1b26ed33b791e6dde9b70b8
tomaszbrauntsch <tbrauntsch@aol.com>
2019-11-25 01:44:57 -0500

    Added-feature-to-get-login-SID-to-demo

--- src/serverlogin.js
+++ src/serverlogin.js
@@ -1,29 +1,30 @@
 //tomaszbrauntsch worked on csgo webscrape and initialized idea on working
 //express for starting app
-//bodyparser to parse body
+//building cookies
 const express = require('express');
 const bodyParser = require('body-parser');
-//webscraping
-const axios = require("axios");
-const cheerio = require('cheerio');
-//json file saving
-const fs = require('fs');
-const http = require('http');
+const cookieParser = require('cookie-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('csgojson'));
 
 var steamid;
 app.post('/csgologin', (req, res) => {
   steamid = req.body.steamid;
-  console.log(`${steamid}`);
+  console.log(`${steamid}`)
+  res.cookie("steamid", steamid);
   //Gets path for html file and opens it up
   path = __dirname + '/csgojson/'
-  res.sendFile(__dirname + '/templates/demo.html')
+  res.sendFile(__dirname + '/templates/demo.html');
 });
 
+app.get('/userData', (req, res)=>{
+  res.send(req.cookies);
+  console.log(req.cookies);
+});
 //Below is used for running not for work
 const port = 8080;
 app.listen(port, () => {
