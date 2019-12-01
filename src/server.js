21c0475873499bf306142ecf4fbd6ed73b5ebfea
tomaszbrauntsch <tbrauntsch@aol.com>
2019-12-01 03:15:06 -0500

    Added-photo-to-demo-Changed-signup-Added-user-input-for-about-me

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const fs = require('fs');
 const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
 app.use(express.static('csgojson'));
 app.use(express.static('scripts'));
 app.use(express.static('static'));
@@ -30,10 +31,13 @@ TODO:
 Work on building a cookie that tells you the status of the loading, of how the
 backend is going. If it is process, good or failed
 */
-
-
+var status = '';
+var aboutMe;
 app.post('/csgosignup', (req, res) => {
+  //building aboutMe Cookie
   var steamid = req.body.steamid;
+  var aboutMe = req.body.aboutMe;
+  status = 'processing';
   var username = '';
   const siteUrl = "https://csgo-stats.com/player/" + steamid;
   console.log(`${siteUrl}`);
@@ -48,13 +52,15 @@ app.post('/csgosignup', (req, res) => {
           steamid : steamid,
           username : $('div.upper div.row div.col-xs-8 div.title-card h1.steam-name').text(),
           image : $('div.upper div.row div.col-xs-8 div.avatar img.img-avatar').attr('src'),
-          rank : $('div.lower div.row div.col-xs-4 span.rank-name').text()
+          rank : $('div.lower div.row div.col-xs-4 span.rank-name').text(),
+          aboutMe : aboutMe
     });
   });
-  userinfo = siteInfo
+  userinfo = siteInfo;
   let data = JSON.stringify(userinfo);
   data = `data = '${data}';`
   fs.writeFileSync(`csgojson/${steamid}.json`, data);
+  status = 'success';
   console.log(userinfo);
   }
 
@@ -62,19 +68,23 @@ app.post('/csgosignup', (req, res) => {
   })
 
   .catch(error => {
+    status = 'failed';
     console.log(error);
   })
-
 });
 
+app.get('/processingstatus', function(req, res){
+  res.send(status);
+});
 //
 //LOGIN FOR CSGO
 //
 
 app.post('/csgologin', (req, res) => {
   steamid = req.body.steamid;
-  console.log(`${steamid}`)
+  console.log(`${steamid}`);
   res.cookie("steamid", steamid);
+  //res.cookie("aboutMe", aboutMe);
   //Gets path for html file and opens it up
   path = __dirname + '/csgojson/'
   res.sendFile(__dirname + '/templates/demo.html');
